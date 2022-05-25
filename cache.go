f3da47bac7f5ff421210cfd670f811bb49993299
global <asgagagsad@gmail.com>
2022-05-25 01:31:22 -0400

    test1.3

--- cache.go
+++ cache.go
@@ -1,31 +1,56 @@
-package cache
+package main
 
 import (
 	"time"
 )
 
 type Cache struct {
-	Key, Value string
-	Deadline   time.Time
+	data map[string]Data
 }
 
-func NewCache(key string, value string, deadline time.Time) Cache {
-	return Cache{Key: key, Value: value, Deadline: deadline}
+type Data struct {
+	value    string
+	deadline time.Time
 }
 
-func (c Cache) Get(key string) (string, bool) {
+func main() {
+	c := NewCache()
+	c.Put("Ключ", "Значение")
+}
+
+func NewCache() Cache {
+	return Cache{
+		data: map[string]Data{},
+	}
+}
+
+func (c *Cache) Get(key string) (string, bool) {
+	for key1, value1 := range c.data {
+		if key == key1 {
+			if value1.deadline.After(time.Now()) {
+				return key1, true
+			} else {
+				return key1, false
+			}
+		}
+	}
 	return "", false
-
 }
 func (c *Cache) Put(key, value string) {
-	NewCache(key, value, time.Now())
-
+	c.data[key] = Data{
+		value:    value,
+		deadline: time.Now().Add(time.Minute),
+	}
 }
 
-func (c Cache) Keys() []string {
-	return nil
+func (c *Cache) Keys() []string {
+	keys := []string{}
+	for key := range c.data {
+		keys = append(keys, key)
+	}
+	return keys
 }
 
-func (c Cache) PutTill(key, value string, deadline time.Time) {
-	NewCache(key, value, deadline)
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.data[key] = Data{value, deadline}
 }
