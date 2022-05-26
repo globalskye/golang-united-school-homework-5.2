298cdf228828f3da2084a06332b30996a286d127
global <asgagagsad@gmail.com>
2022-05-25 22:07:24 -0400

    test1.8 final

--- cache.go
+++ cache.go
@@ -25,9 +25,9 @@ func NewCache() Cache {
 func (c *Cache) Get(key string) (string, bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+
 	for key1, value1 := range c.data {
 		if key == key1 {
-
 			if value1.deadline.After(time.Now()) {
 				return value1.value, true
 			} else {
@@ -42,11 +42,7 @@ func (c *Cache) Get(key string) (string, bool) {
 func (c *Cache) Put(key, value string) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	for key1, _ := range c.data {
-		if key1 == key {
-			key1 = key
-		}
-	}
+
 	c.data[key] = Data{
 		value:    value,
 		deadline: time.Now().Add(time.Hour * 10),
@@ -57,12 +53,10 @@ func (c *Cache) Put(key, value string) {
 func (c *Cache) Keys() []string {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+
 	keys := make([]string, 0, len(c.data))
 	for key := range c.data {
-		if c.data[key].deadline.After(time.Now()) {
-			keys = append(keys, key)
-		}
-
+		keys = append(keys, key)
 	}
 	return keys
 }
@@ -70,6 +64,7 @@ func (c *Cache) Keys() []string {
 func (c *Cache) PutTill(key, value string, deadline time.Time) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+
 	c.data[key] = Data{
 		value:    value,
 		deadline: deadline,
